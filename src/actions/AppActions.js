52a96f8f76b7b3b82245308cb366b6507fc0730c
MaxwellRibeiro <35998964+MaxwellRibeiro@users.noreply.github.com>
2019-05-27 12:49:27 -0300

    252

---
+++ src/actions/AppActions.js
@@ -0,0 +1,16 @@
+import { MODIFICA_ADICIONA_CONTATO_EMAIL } from './types';
+
+export const modificaAdicionaContatoEmail = texto => {
+    return {
+        type: MODIFICA_ADICIONA_CONTATO_EMAIL,
+        payload: texto
+    }
+}
+
+export const adicionaContato = email => {
+    console.log(email);
+
+    return {
+        type: ''
+    }
+}
